7d55b9aaa44137ef752bc1406bf82e8c9ca05f7a
Sonal Kumar <skbrahee@gmail.com>
2024-06-21 13:56:07 -0700

    mout roi

---
+++ crop_mouth/crop_mouth_script.py
@@ -0,0 +1,24 @@
+import os
+from tqdm import tqdm
+import pickle
+
+dir_path = ""
+txt_file_path = ""
+mp4_files = []
+for root, dirs, files in tqdm(os.walk(dir_path)):
+    for file in files:
+        if file.endswith('.mp4'):
+            mp4_files.append(os.path.join(root, file))
+
+if not mp4_files:
+    with open(txt_file_path, "rb") as fp:   # Unpickling
+        mp4_files = pickle.load(fp)
+
+
+print(len(mp4_files))
+
+for i in tqdm(mp4_files):
+    src_path = i
+    dest_path = i[:-4] + '_crop.mp4'
+    print(src_path, dest_path)
+    os.system(f'python crop_mouth.py data_filename={src_path} dst_filename={dest_path}')
