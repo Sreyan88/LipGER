adefbfb7f07629b64ee77bca78c33432592d58e2
Sreyan Ghosh <gsreyan@gmail.com>
2024-07-08 23:49:44 -0400

    minor change

--- infer.sh
+++ infer.sh
@@ -2,4 +2,4 @@
 
 # "test_data" specifies the test set, e.g., test_chime4/test_real, test_chime4/test_simu
 
-python inference/robust_ger.py --test_data /absolute/path/to_your_test.pt
+python inference/lipger.py --test_data /absolute/path/to_your_test.pt
